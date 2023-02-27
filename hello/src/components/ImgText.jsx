65a5c1ca1ef080dd42932ba0acfc47a05a2bc0f2
Pilar Terron <pilarterronmartin@gmail.com>
2023-02-27 18:28:38 +0100

    lab Ok

--- hello/src/components/ImgText.jsx
+++ hello/src/components/ImgText.jsx
@@ -2,14 +2,13 @@
 
 function ImgText({img,title,textImg}){
 
+
     return(
       <div id="containerTwo">
             <img src={img} alt="img" />
             <h2>{title}</h2>
             <p>{textImg}</p>
       </div>
-     
- 
     )
 }
 
